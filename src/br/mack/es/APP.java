bcdb3253f6ded1976daead3db8f1afcd6e286468
31614957 <31614957@087928-P33S103.macklabs.br>
2017-10-31 09:57:47 -0200

    add variavel y

--- src/br/mack/es/APP.java
+++ src/br/mack/es/APP.java
@@ -2,6 +2,7 @@ package br.mack.es;
 public class APP {
     public static void main(String[] args) {
         System.out.println("aa");
-        int x = 0;
+        int x = 10;
+        int y = 15;
     }
 }
